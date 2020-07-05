cf3ffd5417888ed80a17aa3a420c70a57eca0db6
Felipe Merchan <felipe-sc@hotmail.es>
2020-07-05 17:26:36 -0500

    Updates

---
+++ precache-manifest.426d060aecb6db3bc4a30f63aa31c9c0.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d2d503f3f59210826dc2dd880bf0a2",
+    "url": "/platzi-master-frontend-exercises/index.html"
+  },
+  {
+    "revision": "5b8b8f64c4df558e2a15",
+    "url": "/platzi-master-frontend-exercises/static/css/main.e6c3df09.chunk.css"
+  },
+  {
+    "revision": "697bc06f194f72e27ae7",
+    "url": "/platzi-master-frontend-exercises/static/js/2.7513f9f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/platzi-master-frontend-exercises/static/js/2.7513f9f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8b8f64c4df558e2a15",
+    "url": "/platzi-master-frontend-exercises/static/js/main.690d9ca3.chunk.js"
+  },
+  {
+    "revision": "67ce0659309e5c12af30",
+    "url": "/platzi-master-frontend-exercises/static/js/runtime-main.f94e12dd.js"
+  },
+  {
+    "revision": "229302749a398e34eab80749b9d2934c",
+    "url": "/platzi-master-frontend-exercises/static/media/analisis.22930274.png"
+  },
+  {
+    "revision": "8e852252912655f4e598280faec7a63c",
+    "url": "/platzi-master-frontend-exercises/static/media/arrows.8e852252.svg"
+  },
+  {
+    "revision": "69638b3a68bf01cdc78cca9352c781ac",
+    "url": "/platzi-master-frontend-exercises/static/media/barack-obama.69638b3a.png"
+  },
+  {
+    "revision": "de6a35dc4a29bfdeb08c092f8cc2d12a",
+    "url": "/platzi-master-frontend-exercises/static/media/brand.de6a35dc.JPG"
+  },
+  {
+    "revision": "dd1df017c7da0e129bbbbb6d922da6be",
+    "url": "/platzi-master-frontend-exercises/static/media/bullet-points.dd1df017.svg"
+  },
+  {
+    "revision": "eabe4f8c394086834eb9016e911c8431",
+    "url": "/platzi-master-frontend-exercises/static/media/cat.eabe4f8c.jpg"
+  },
+  {
+    "revision": "7e76a94020425cb3b92fe35ff9011b99",
+    "url": "/platzi-master-frontend-exercises/static/media/consultoria.7e76a940.png"
+  },
+  {
+    "revision": "6435cb9173d91e3caf9f2ecbe086cf46",
+    "url": "/platzi-master-frontend-exercises/static/media/cuarta-semana-poster.6435cb91.png"
+  },
+  {
+    "revision": "f18d828a3d97ec758445a5194335c882",
+    "url": "/platzi-master-frontend-exercises/static/media/facebook.f18d828a.svg"
+  },
+  {
+    "revision": "c07d986728581740ee50ac39280d510f",
+    "url": "/platzi-master-frontend-exercises/static/media/featured-entry.c07d9867.jpg"
+  },
+  {
+    "revision": "39e7cd0e189e8d822b391fa8b402f6ef",
+    "url": "/platzi-master-frontend-exercises/static/media/felipe-sc-imagotipo.39e7cd0e.svg"
+  },
+  {
+    "revision": "4e8ce4e4c7bd2bff0636b39c7b6f55c9",
+    "url": "/platzi-master-frontend-exercises/static/media/felipe-sc-photo@2x.4e8ce4e4.png"
+  },
+  {
+    "revision": "f5cb356f7efcb8e9a92617cc58bc032e",
+    "url": "/platzi-master-frontend-exercises/static/media/formacion.f5cb356f.png"
+  },
+  {
+    "revision": "da4c1c2f6da55f2c8ece97f34fa33a19",
+    "url": "/platzi-master-frontend-exercises/static/media/investigacion.da4c1c2f.png"
+  },
+  {
+    "revision": "ba10dbbbda3b257d7269c47dfc27cd18",
+    "url": "/platzi-master-frontend-exercises/static/media/picture.ba10dbbb.png"
+  },
+  {
+    "revision": "927008463175a6d000a2cfdab2c2955e",
+    "url": "/platzi-master-frontend-exercises/static/media/primera-semana-poster.92700846.png"
+  },
+  {
+    "revision": "e6757da03fed1a1dbba7f16c1edc366c",
+    "url": "/platzi-master-frontend-exercises/static/media/quinta-semana-poster.e6757da0.png"
+  },
+  {
+    "revision": "02111f20632785e1364b7cfc0741bf99",
+    "url": "/platzi-master-frontend-exercises/static/media/scroll-down-arrow.02111f20.svg"
+  },
+  {
+    "revision": "f98d3df4002af0073b0bc347ae2284b4",
+    "url": "/platzi-master-frontend-exercises/static/media/sexta-semana-poster.f98d3df4.png"
+  },
+  {
+    "revision": "68aac7b10a8da268243ec2cd3082305c",
+    "url": "/platzi-master-frontend-exercises/static/media/spotify.68aac7b1.svg"
+  },
+  {
+    "revision": "f31a786990bcb8f0de80ca8e2c0080db",
+    "url": "/platzi-master-frontend-exercises/static/media/the-new-york-times-logotipo.f31a7869.svg"
+  },
+  {
+    "revision": "ca8b79d3d68c2aff5ed993e9a2e79bae",
+    "url": "/platzi-master-frontend-exercises/static/media/twitter.ca8b79d3.svg"
+  },
+  {
+    "revision": "93e2a0676014a4eaaf1511eefb787874",
+    "url": "/platzi-master-frontend-exercises/static/media/youtube.93e2a067.svg"
+  }
+]);
